13e7d069f5487a11ae7f0f0171fd7fb68155313b
Thana B. <thana.b@gscale.jp>
2025-02-17 08:28:47 +0000

    test config for https

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,6 @@
 import reflex as rx
 from enum import Enum
+from reflex.state import StateManager
 
 
 class LogLevel(str, Enum):
@@ -10,8 +11,16 @@ class LogLevel(str, Enum):
     CRITICAL = "critical"
 
 
+from reflex import constants
+
 config = rx.Config(
     app_name="chatapp",
     loglevel=LogLevel.DEBUG,
     env=rx.Env.DEV,
+    state_manager_mode=constants.StateManagerMode.MEMORY,
+    # env=rx.Env.PROD,
+    # frontend_port=80,
+    # backend_host="152.42.211.214",
+    # api_url="http://152.42.211.214:8000",
+    # deploy_url="http://152.42.211.214:80",
 )
